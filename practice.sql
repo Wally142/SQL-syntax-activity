7f9a64a75e5e0f3fca102c4531b88f95e3a0459a
Greg.Wallerus <gwallerus@gmail.com>
2017-09-12 16:58:36 -0500

    sql practice

---
+++ practice.sql
@@ -0,0 +1,18 @@
+-- Get all users from Chicago.
+SELECT * FROM syntax_practice WHERE city ='chicago';
+-- Get all users with usernames that contain the letter a.
+SELECT * FROM syntax_practice WHERE username LIKE'%a%'
+-- The bank is giving a new customer bonus! Update all records with an account balance of 0.00 and a transactions_attempted of 0. Give them a new account balance of 10.00.
+UPDATE syntax_practice SET account_balance = '10.00' WHERE account_balance='0.00' AND transactions_attempted =0;
+-- Select all users that have attempted 9 or more transactions.
+SELECT * FROM syntax_practice WHERE transactions_attempted >= 9;
+-- Get the username and account balance of the 3 users with the highest balances, sort highest to lowest balance. NOTE: Research LIMIT
+SELECT * FROM syntax_practice WHERE account_balance >355 ORDER BY account_balance DESC;
+-- Get the username and account balance of the 3 users with the lowest balances, sort lowest to highest balance.
+SELECT * FROM syntax_practice WHERE account_balance < 11;
+-- Get all users with account balances that are more than $100.
+SELECT * FROM syntax_practice WHERE account_balance > 100;
+-- Add a new record.
+INSERT INTO syntax_practice (username, city, transactions_completed, transactions_attempted, account_balance) VALUES ('greg', 'minneapolis', 12, 14, 250000);
+-- The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: Delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
+DELETE FROM syntax_practice WHERE city ='miami' or city = 'phoenix' and  transactions_completed < 5;
